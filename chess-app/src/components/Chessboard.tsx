2d36ee272de3947d97e1e6d2996d847c17198159
ujjwal7014 <ujjwal7014@gmail.com>
2022-08-08 01:36:18 +0530

    basic layout setup

---
+++ chess-app/src/components/Chessboard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Chessboard.css";
+
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "h"];
+  
+export default function Chessboard() {
+  let board = [];
+
+  return (
+    <div id="chessboard">
+        
+    </div>
+  )
+}
